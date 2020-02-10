72b07c016dd7c3f5bb6486cae261fd56bf9ccde8
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-02-10 14:56:15 -0600

    Added rules for small straight and full house

--- src/Die.js
+++ src/Die.js
@@ -2,12 +2,24 @@ import React, { Component } from 'react';
 import './Die.css';
 
 class Die extends Component {
+  //Using constructor to allow a binding function
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  //Calling handleclick here as its own function rather than using an arrow function in the render
+  //as a best practice; remember calling arrow function in render could cause performance issue in larger apps
+  handleClick() {
+    this.props.handleClick(this.props.idx);
+  }
   render() {
     return (
       <button
         className={'Die'}
         style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
-        onClick={() => this.props.handleClick(this.props.idx)}
+        onClick={this.handleClick}
       >
         {this.props.val}
       </button>
