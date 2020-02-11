92babba23e678cd9751852b734f2270f2c257990
Silas Groskreutz <50895337+silasgroskreutz@users.noreply.github.com>
2020-02-10 19:45:42 -0600

    game completed

--- src/Die.js
+++ src/Die.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import './Die.css';
 
 class Die extends Component {
+  //Using staic props
+  //...could be placed in constructor for it to work
+  static defaultProps = {
+    numberWords: ['one', 'two', 'three', 'four', 'five', 'six']
+  };
+
   //Using constructor to allow a binding function
   constructor(props) {
     super(props);
@@ -15,14 +21,12 @@ class Die extends Component {
     this.props.handleClick(this.props.idx);
   }
   render() {
+    const { numberWords, locked, val, disabled, rolling } = this.props;
+    let classes = `Die fas fa-dice-${numberWords[val - 1]} fa-5x `;
+    if (locked) classes += 'Die-locked ';
+    if (rolling) classes += 'Die-rolling';
     return (
-      <button
-        className={'Die'}
-        style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
-        onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
+      <i className={classes} onClick={this.handleClick} disabled={disabled}></i>
     );
   }
 }
